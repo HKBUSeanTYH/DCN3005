cb1853f19b53786d1f714db92b1678638779de69
HKBUSeanTYH <seantyh118@gmail.com>
2020-11-11 20:49:18 +0800

    making preliminary classes

--- src/StartUp.java
+++ src/StartUp.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
 
 public class StartUp {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please input\nServer IP: ");
+		String ip = in.nextLine().trim();
+		
 		System.out.println("Please input your password");
+		String username = in.nextLine();
+		
+		System.out.println("Please input your password");
+		String pw = in.nextLine();
 		
 		//need to put UDP and TCP on startup codes
 		
-		String pw = in.nextLine();
-		System.out.println(pw);
+		System.out.println(username+": "+pw);
 		in.close();
 	}
 
---
+++ src/TCP.java
@@ -0,0 +1,16 @@
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TCP {
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+
+	}
+
+}
