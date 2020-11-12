52a5a47a2a18c9c30bdb09ffb0072913e0ace0e8
HKBUSeanTYH <seantyh118@gmail.com>
2020-11-12 21:56:38 +0800

    working on tcpClient.

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,5 +1,38 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class TCPClient {
+
+	public TCPClient(String serverIP, int port) throws IOException{
+		Socket socket = new Socket(serverIP, port);
+
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		Scanner in = new Scanner(System.in); // take user input and send to server
+		
+		boolean login = false;
+		
+		while (!login) {
+			System.out.println("Please input your username");
+			String username = in.nextLine();
+			System.out.println("Please input your password");
+			String pw = in.nextLine();
+			System.out.println("Connecting to server...");
+			
+			sendLogin(username, pw, out);
+
+		}
+	}
+	
+	public void sendLogin(String username, String pw, DataOutputStream out) {
+		
+	}
+
+	public void sendCommand(DataOutputStream out) throws IOException {
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
